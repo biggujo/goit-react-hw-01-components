e50ffa604473e0ef14f0fa1b41a3f7e4971dd06b
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-08-10 23:40:22 +0300

    Add theming

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './utils/theme';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
-);
+  </ThemeProvider>
+</React.StrictMode>);
---
+++ src/utils/theme.js
@@ -0,0 +1,5 @@
+export const theme = {
+  shadow: {
+    primary: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',
+  },
+};
