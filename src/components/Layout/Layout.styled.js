b008d1393af95015869cf6035842895022ec6373
Dmytro Ivashchenko <gen.style22@gmail.com>
2023-08-10 21:57:19 +0300

    Add page layout

---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  max-width: 900px;
+  padding: 10px;
+
+  margin-inline: auto;
+`;
